5cf7b23dc47e53bb11f9c09bae162610fbb93c38
Jeferson W. V. Silva <jefersonwillians137@gmail.com>
2020-03-31 10:07:05 -0400

    video_237_ClasseDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
 	
+	Department obj = new Department(2, "Books");
 	
+	System.out.println(obj);
     }
     
 }
