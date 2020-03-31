e6015a85d76b011c9c73e4556b1e8239f6c1f72c
Jeferson W. V. Silva <jefersonwillians137@gmail.com>
2020-03-31 10:22:35 -0400

    video_238_ClasseSeller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
 	
 	Department obj = new Department(2, "Books");
+	Seller obj2 = new Seller(2647, "Jeferson Willian", "jwvs@gmail.com", new Date(), 50000.00, obj);
 	
 	System.out.println(obj);
+	System.out.println(obj2);
+	
     }
     
 }
